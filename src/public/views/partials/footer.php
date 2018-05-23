2d5dfbd9cd18b20b015b1968ec94086d0a509adc
TokAhmet <ahmet.ordulu@hotmail.se>
2018-05-23 13:44:49 +0200

    footer name change

--- src/public/views/partials/footer.php
+++ src/public/views/partials/footer.php
@@ -1,6 +1,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="../scripts/title.js"></script>
+  <script src="../scripts/search.js"></script>
   <script src="../scripts/likes.js"></script>
   <script src="../scripts/comments.js"></script>
   <script src="../scripts/main.js"></script>
